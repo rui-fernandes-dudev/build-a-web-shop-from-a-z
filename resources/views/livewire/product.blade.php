693d0a9245ae29010011c5866fe500509072d23c
rui-fernandes-dudev <rui.fernandes@dudev.pt>
2024-07-09 15:47:41 +0100

    Dynamic Product Image Selector

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,13 +1,13 @@
 <div class="grid grid-cols-2 gap-10 my-12">
-    <div class="space-y-4">
+    <div class="space-y-4" x-data="{ image: '/{{ $this->product->image->path }}' }">
         <div class="bg-white p-5 rounded-lg shadow">
-            <img src="/{{ $this->product->image->path }}" alt="">
+            <img x-bind:src="image" alt="">
         </div>
 
         <div class="grid grid-cols-4 gap-4">
             @foreach($this->product->images as $image)
                 <div class="rounded bg-white p-2 shadow">
-                    <img src="/{{ $image->path }}" class="rounded" alt="">
+                    <img src="/{{ $image->path }}" @click="image = '/{{ $image->path }}'" class="rounded" alt="">
                 </div>
             @endforeach
         </div>
