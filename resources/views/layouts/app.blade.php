19b42a1e2afdfbc68b65cf0774305914ccba6829
rui-fernandes-dudev <rui.fernandes@dudev.pt>
2024-07-09 13:44:47 +0100

    designing-our-storefront

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="max-w-4xl mx-auto">
                 {{ $slot }}
             </main>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Livewire\StoreFront::class)->name('home');
 
-Route::middleware([
+/*Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
@@ -14,4 +12,4 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
+});*/
