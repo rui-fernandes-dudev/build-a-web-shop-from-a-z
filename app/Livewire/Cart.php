922c8124ed9e74df4cbefd8ada778737423e23f7
rui-fernandes-dudev <rui.fernandes@dudev.pt>
2024-07-10 15:53:13 +0100

    Session Cart to User Cart Migration

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -7,19 +7,24 @@ use Livewire\Component;
 
 class Cart extends Component
 {
+    public function getCartProperty()
+    {
+        return CartFactory::make()->loadMissing(['items', 'items.product', 'items.variant']);
+    }
+
     public function getItemsProperty()
     {
-        return CartFactory::make()->items;
+        return $this->cart->items;
     }
 
     public function increment($itemId)
     {
-        CartFactory::make()->items()->find($itemId)?->increment('quantity');
+        $this->cart->items->find($itemId)?->increment('quantity');
     }
 
     public function decrement($itemId)
     {
-        $item = CartFactory::make()->items()->find($itemId);
+        $item = $this->cart->items->find($itemId);
 
         if($item->quantity > 1)
         {
@@ -29,7 +34,7 @@ class Cart extends Component
 
     public function delete($itemId)
     {
-        CartFactory::make()->items()->where('id', $itemId)->delete();
+        $this->cart->items()->where('id', $itemId)->delete();
 
         $this->dispatch('productRemovedFromCart');
     }
